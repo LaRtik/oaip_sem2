dd99d26723640e6ed62aa5d1132104e85de21d46
Ilya Lazuk <36516154+LaRtik@users.noreply.github.com>
2021-06-02 22:17:50 +0300

    Add files via upload

---
+++ lab6/Unit4.h
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit4H
+#define Unit4H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ComCtrls.hpp>
+#include <Vcl.Grids.hpp>
+//---------------------------------------------------------------------------
+class TForm4 : public TForm
+{
+__published:	// IDE-managed Components
+	TTreeView *TreeView1;
+	TStringGrid *StringGrid1;
+	TButton *Button1;
+	TButton *Button2;
+	TMemo *Memo1;
+	TButton *Button3;
+	TButton *Button4;
+	TButton *Button5;
+	TLabel *Label1;
+	TLabel *Label2;
+	TButton *Button6;
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall Button3Click(TObject *Sender);
+	void __fastcall Button4Click(TObject *Sender);
+	void __fastcall Button5Click(TObject *Sender);
+	void __fastcall Button6Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm4(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm4 *Form4;
+//---------------------------------------------------------------------------
+#endif
